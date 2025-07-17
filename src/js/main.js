9a6852f93b817594b2706917e7d33a0a2d56b006
Sanjeevan Rames <thesilverghost2005@gmail.com>
2025-07-17 16:19:37 +0800

    code to play a round of rock paper scissors

--- src/js/main.js
+++ src/js/main.js
@@ -32,6 +32,35 @@ function getHumanChoice(){
 				break;
 		}
 	}
-
 }
-console.log(getHumanChoice());
+
+function playRound(computerChoice,humanChoice){
+	let computerScore = 0;
+	let humanScore = 0;
+	let messageTemplate = `Computer picked ${computerChoice}.
+	You picked ${humanChoice}.\n`
+	if (computerChoice == humanChoice){
+		console.log(`${messageTemplate} This round is a tie!`);
+		return [computerScore,humanScore];
+	}
+	else if (computerChoice == "Rock" && humanChoice = "Scissors" ||
+	computerChoice == "Paper" && humanChoice = "Rock" ||	
+	computerChoice == "Scissors" && humanChoice = "Paper"){
+		console.log(`${messageTemplate} Computer wins this round!`);
+		computerScore += 1;
+		console.log(`The score as of now:
+		Computer: ${computerScore}
+		You: ${humanScore}`);
+		return [computerScore,humanScore];
+	}
+	else{
+		console.log(`${messageTemplate} You win this round`);
+		humanScore += 1;
+		console.log(`The score as of now:
+		Computer: ${computerScore}
+		You: ${humanScore}`);
+		return [computerScore,humanScore];
+	}
+}
+
+
