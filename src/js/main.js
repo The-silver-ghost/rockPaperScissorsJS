ba34e4bd2d3019139f17c16ce9f31183b72ae9f9
Sanjeevan Rames <thesilverghost2005@gmail.com>
2025-07-17 16:47:38 +0800

    code logic to play 5 rounds, count score and determine victory

--- src/js/main.js
+++ src/js/main.js
@@ -35,32 +35,54 @@ function getHumanChoice(){
 }
 
 function playRound(computerChoice,humanChoice){
-	let computerScore = 0;
-	let humanScore = 0;
 	let messageTemplate = `Computer picked ${computerChoice}.
 	You picked ${humanChoice}.\n`
 	if (computerChoice == humanChoice){
 		console.log(`${messageTemplate} This round is a tie!`);
-		return [computerScore,humanScore];
+		return "tie";
 	}
-	else if (computerChoice == "Rock" && humanChoice = "Scissors" ||
-	computerChoice == "Paper" && humanChoice = "Rock" ||	
-	computerChoice == "Scissors" && humanChoice = "Paper"){
+	else if (computerChoice == "Rock" && humanChoice == "Scissors" ||
+	computerChoice == "Paper" && humanChoice == "Rock" ||	
+	computerChoice == "Scissors" && humanChoice == "Paper"){
 		console.log(`${messageTemplate} Computer wins this round!`);
-		computerScore += 1;
-		console.log(`The score as of now:
-		Computer: ${computerScore}
-		You: ${humanScore}`);
-		return [computerScore,humanScore];
+		return "computer";
 	}
 	else{
 		console.log(`${messageTemplate} You win this round`);
-		humanScore += 1;
-		console.log(`The score as of now:
-		Computer: ${computerScore}
-		You: ${humanScore}`);
-		return [computerScore,humanScore];
+		return "player";
 	}
 }
 
+//main 
+let playerScore = 0, computerScore = 0;
+for (rounds = 0; rounds < 5; rounds += 1){
+	console.log(`Round ${rounds + 1}`);
+	let result = playRound(generateComputerChoice(),getHumanChoice());
+	if (result == "computer"){
+		computerScore += 1;
+		console.log(`The score as of now:
+		Computer: ${computerScore}
+		You: ${playerScore}`);
+	}
+	else if (result == "player"){
+		playerScore += 1;
+		console.log(`The score as of now:
+		Computer: ${computerScore}
+		You: ${playerScore}`);
+	}
+	else {
+	console.log(`The score as of now:
+	Computer: ${computerScore}
+	You: ${playerScore}`);	
+	}
+}
 
+if (computerScore > playerScore){
+	console.log(`The Computer Wins!`);
+}
+else if (playerScore > computerScore){
+	console.log(`You win!`);
+}
+else{
+	console.log(`It's a tie!`);
+}
