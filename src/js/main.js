540791e5200df60aca9f251dd135766027fd9953
Sanjeevan Rames <thesilverghost2005@gmail.com>
2025-07-17 11:35:47 +0800

    code to generate computer choice

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
-console.log("Hello")
+function generateComputerChoice(){
+	let randomChoice = Math.floor(Math.random() * 3); //0 <= randomChoice <=2
+	switch (randomChoice){
+		case 0:
+			return "Rock";
+			break;
+		case 1:
+			return "Paper";
+			break;
+		case 2:
+			return "Scissors";
+			break;
+	}
+}
+
+console.log(generateComputerChoice());
